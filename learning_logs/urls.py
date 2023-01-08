ebc24d9af34e0a6836aa3a8224c19c8eec7f66a4
0xbm <0xmb@protonmail.com>
2023-01-08 13:39:28 +0100

    add urls to start

---
+++ learning_logs/urls.py
@@ -0,0 +1,7 @@
+# learning_logs
+from django.urls import path
+from . import views
+
+app_name = "learning_logs"
+
+urlpatterns = [path("", views.index, name="index")]
---
+++ urls.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [url("admin/", admin.site.urls), url("", include("learning_logs.urls"))]
