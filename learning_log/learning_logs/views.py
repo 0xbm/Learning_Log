046bc0cdea22672e1533f014af0c51224acf5505
0xbm <0xmb@protonmail.com>
2023-03-09 17:58:41 +0100

    files in app

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Topic
+
+
+def index(request):
+    return render(request, "learning_logs/index.html")
+
+
+def topics(request):
+    """Wyswietlenie wszytskich tematow"""
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics": topics}
+    return render(request, "learning_logs/topics.html", context)
