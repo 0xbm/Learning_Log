09467ba18bc4e7821a713c126c7b102bb1aabf9c
0xbm <0xmb@protonmail.com>
2023-03-15 18:28:12 +0100

    add button 'new_topic'

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -16,8 +17,22 @@ def topics(request):
 
 
 def topic(request, topic_id):
-    "wyswietla pojedynczy temat i wszystkie powaizane z nim wpisy"
+    """wyswietla pojedynczy temat i wszystkie powaizane z nim wpisy"""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
+
+
+def new_topic(request):
+    """dodaj nowy temat"""
+    if request.method != "POST":
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topics")
+
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
