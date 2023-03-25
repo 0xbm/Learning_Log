6b0c6faf08ffbfe7a19a1f1b252dfef076c96669
0xbm <0xmb@protonmail.com>
2023-03-25 11:40:42 +0100

    add option 'edit_entry'

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 
@@ -55,3 +55,22 @@ def new_entry(request, topic_id):
     # Wyswietlanie pustego formularza
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
+
+
+def edit_entry(request, entry_id):
+    """Edycja istniejacego wpisu"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != "POST":
+        # Zadanie poczatkowe, wypelnienie formularza trescia
+        form = EntryForm(instance=entry)
+    else:
+        # Przekazanie danych za pomoca metody POST
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topic", topic_id=topic.id)
+
+    context = {"entry": entry, "topic": topic, "form": form}
+    return render(request, "learning_logs/edit_entry.html", context)
